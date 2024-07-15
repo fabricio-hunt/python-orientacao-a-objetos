88101350c71f83be3e8c70deb32f119a91f8125a
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-07-15 20:17:54 -0300

    atualização com funções

--- Python crie a sua primeira aplicação/app.py
+++ Python crie a sua primeira aplicação/app.py
@@ -1,6 +1,8 @@
 import os
 
-print("""
+def exibir_nome_do_programa():
+
+     print("""
       
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -10,25 +12,39 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: ')) 
-print(f'Você escolheu a opção {opcao_escolhida}')
+def exibir_opcoes():
+     print('1. Cadastrar restaurante')
+     print('2. Listar restaurante')
+     print('3. Ativar restaurante')
+     print('4. Sair\n')
 
 def finalizar__app():
      os.system('cls')
      print('Sistema Encerrado!!!\n')
 
-if opcao_escolhida == 1:
-    print('Cadastar Restaurante')
-elif opcao_escolhida == 2:
-     print('Listar restaurante')
-elif opcao_escolhida == 3:
-     print('tivar restaurante')
-else:
-     finalizar__app()
+
+def escolher_opcoes():
+     opcao_escolhida = int(input('Escolha uma opção: ')) 
+     print(f'Você escolheu a opção {opcao_escolhida}')
+
+     if opcao_escolhida == 1:
+          print('Cadastar Restaurante')
+     elif opcao_escolhida == 2:
+          print('Listar restaurante')
+     elif opcao_escolhida == 3:
+          print('tivar restaurante')
+     else:
+          finalizar__app()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+    
+if __name__ == "__main__":
+    main()
+
+
 
 
