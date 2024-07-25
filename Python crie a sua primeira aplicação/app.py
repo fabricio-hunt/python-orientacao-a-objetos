758c179490e4e56bc7b9bab739a190b6530f66a9
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-07-25 19:50:36 -0300

    validação do menu

--- Python crie a sua primeira aplicação/app.py
+++ Python crie a sua primeira aplicação/app.py
@@ -24,23 +24,33 @@ def finalizar__app():
 
 
 def escolher_opcoes():
-     opcao_escolhida = int(input('Escolha uma opção: ')) 
-     print(f'Você escolheu a opção {opcao_escolhida}')
+     try:
+          opcao_escolhida = int(input('Escolha uma opção: ')) 
+          print(f'Você escolheu a opção {opcao_escolhida}')
 
-     if opcao_escolhida == 1:
-          print('Cadastar Restaurante')
-     elif opcao_escolhida == 2:
-          print('Listar restaurante')
-     elif opcao_escolhida == 3:
-          print('tivar restaurante')
-     else:
-          finalizar__app()
-
-
+          if opcao_escolhida == 1:
+               print('Cadastar Restaurante')
+          elif opcao_escolhida == 2:
+               print('Listar restaurante')
+          elif opcao_escolhida == 3:
+               print('tivar restaurante')
+          elif opcao_escolhida == 4:
+               finalizar__app()
+          else:
+               opcao_invalida()
+     except:
+          opcao_invalida()
+          
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcoes()
+
+def opcao_invalida():
+     print('Opção Inválida\n')
+     input('Digite qualquer tecla para voltar ao Menu principal!')
+     main()
     
 if __name__ == "__main__":
     main()
