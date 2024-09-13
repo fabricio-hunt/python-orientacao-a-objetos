fd774b2170be523c13a820a9037beaac7bd0fa4b
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-12 23:17:45 -0300

    construtor

--- Python OO/modelos/main.py
+++ Python OO/modelos/main.py
@@ -5,19 +5,22 @@
 #Situação (Se ativo / inativo)
 
 class Restaurante:
-    nome = ''
-    categoria  = ''
-    situacao = False
+    def __init__(self, nome, categoria):
+        self.nome = ''
+        self.categoria  = ''
+        self.situacao = False
 
 
-restaurante_sabor_de_queimado = Restaurante()
+restaurante_sabor_de_queimado = Restaurante('Sabor de Queimado', 'Churrascaria')
 restaurante_sabor_de_queimado.nome = 'Sabor de Queimado'
 restaurante_sabor_de_queimado.categoria = 'Churrascaria'
 
 
-churras_rest = Restaurante()
+churras_rest = Restaurante('Churras Rest', 'Churrascaria')
 churras_rest.nome = 'Churras Rest'
 churras_rest.categoria = 'Churrascaria'
 
 
 restaurantes = [restaurante_sabor_de_queimado, churras_rest]
+
+print(vars(restaurante_sabor_de_queimado))
