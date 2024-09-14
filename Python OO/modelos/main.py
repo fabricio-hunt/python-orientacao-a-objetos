2ce04268ffbd71966b6e3ab4ff99f32fa8a7ae33
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-13 23:24:09 -0300

    criando meus metodos

--- Python OO/modelos/main.py
+++ Python OO/modelos/main.py
@@ -5,25 +5,33 @@
 #Situação (Se ativo / inativo)
 
 class Restaurante:
+    restaurantes = [] 
+
     def __init__(self, nome, categoria):
         self.nome = ''
         self.categoria  = ''
         self.situacao = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.situacao}')
     
 
 restaurante_sabor_de_queimado = Restaurante('Sabor de Queimado', 'Churrascaria')
 restaurante_sabor_de_queimado.nome = 'Sabor de Queimado'
 restaurante_sabor_de_queimado.categoria = 'Churrascaria'
 
-
 churras_rest = Restaurante('Churras Rest', 'Churrascaria')
 churras_rest.nome = 'Churras Rest'
 churras_rest.categoria = 'Churrascaria'
 
+tomy = Restaurante('Tomy', 'Peixaria')
+tomy.nome = 'Tomy'
+tomy.categoria = 'Peixaria'
 
-restaurantes = [restaurante_sabor_de_queimado, churras_rest]
 
-print(restaurante_sabor_de_queimado)
+Restaurante.listar_restaurantes()
