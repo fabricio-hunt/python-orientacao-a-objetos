9d42edc998d5232fec5532954c03d356b25dd4a7
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-16 20:52:37 -0300

    propriedades

--- Python OO/modelos/main.py
+++ Python OO/modelos/main.py
@@ -8,13 +8,13 @@ class Restaurante:
     restaurantes = [] 
 
     def __init__(self, nome, categoria):
-        self.nome = ''
-        self.categoria  = ''
+        self._nome = nome.title()
+        self.categoria  = categoria
         self._situacao = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do Restaurante.'.ljust(25)} | {'Categotia'.ljust(25)} | {'Status'}')
@@ -27,15 +27,15 @@ class Restaurante:
 
 
 restaurante_sabor_de_queimado = Restaurante('Sabor de Queimado', 'Churrascaria')
-restaurante_sabor_de_queimado.nome = 'Sabor de Queimado'
+restaurante_sabor_de_queimado._nome = 'Sabor de Queimado'
 restaurante_sabor_de_queimado.categoria = 'Churrascaria'
 
 churras_rest = Restaurante('Churras Rest', 'Churrascaria')
-churras_rest.nome = 'Churras Rest'
+churras_rest._nome = 'Churras Rest'
 churras_rest.categoria = 'Churrascaria'
 
 tomy = Restaurante('Tomy', 'Peixaria')
-tomy.nome = 'Tomy'
+tomy._nome = 'Tomy'
 tomy.categoria = 'Peixaria'
 
 
